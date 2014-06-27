3347f6648e65bffaf3196f6cb2accee8168a91f0
JordanBeauchamp <beauchjord@gmail.com>
2014-06-27 14:55:29 +1200

    Improvements to the sprintboard view

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,11 +1,27 @@
 class SprintsController < ApplicationController
+  before_filter :set_cache_buster
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
   def index
 	 @sprints = Sprint.all
   end
 
   def show
-    puts params[:id]
 	 @sprint = Sprint.find(params[:id])
+   @tasks = Hash.new
+
+   # Create map of user stories and their tasks.
+   @sprint.user_stories.each do |user_story|
+    ready_tasks = user_story.tasks.select{ |task| task[:status] == TaskStatus::READY}
+    in_progress_tasks = user_story.tasks.select{ |task| task[:status] == TaskStatus::IN_PROGRESS}
+    done_tasks = user_story.tasks.select{ |task| task[:status] == TaskStatus::DONE}
+    @tasks[user_story.id.to_s] = {'Ready' => ready_tasks, 'In Progress' => in_progress_tasks, 'Done' => done_tasks}
+   end
   end
 
   def new
